4f196525dbea693c60d5b571bbb785834cfe95a7
finnxie <finnxie@tencent.com>
2022-02-10 16:17:51 +0800

    feat: modify p5

--- leetcode/solutions/p5/solution.go
+++ leetcode/solutions/p5/solution.go
@@ -2,18 +2,15 @@ package p5
 
 // LongestPalindrome 最长回文
 func LongestPalindrome(s string) string {
-	if len(s) == 1 {
-		return s
-	}
 	var palindrome string
-	for i := 0; i < len(s)-1; i++ {
+	for i := 0; i < len(s); i++ {
 		l, r := i, i
-		for j := i + 1; j < len(s); j++ {
-			if s[i] != s[j] {
+		for ; r < len(s); r++ {
+			if s[i] != s[r] {
 				break
 			}
-			r = j
 		}
+		r = r - 1
 
 		for {
 			if l-1 < 0 || r+1 >= len(s) {
